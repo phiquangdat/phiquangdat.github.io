18aae0cdf07f2ca6b4177bb472b1a3494681bce4
phiquangdat <datphi020603@gmail.com>
2025-07-24 00:50:09 +0300

    Implement movie fetching and display on MoviePage component, enhancing UI with responsive layout and styling.

--- frontend/src/components/MoviePage.tsx
+++ frontend/src/components/MoviePage.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getMovies, getFavoriteMovies } from "../services/movieApi";
 
 export default function MoviePage() {
+  const [movies, setMovies] = useState<any[]>([]);
+
+  useEffect(() => {
+    getFavoriteMovies().then((movies) => {
+      setMovies(movies);
+    });
+  }, []);
+
   return (
-    <div>
-      <h1>MoviePage</h1>
+    <div className="mx-auto px-4 pt-16">
+      <h1 className="text-center font-bold text-2xl text-[#000]">Movie Page</h1>
+
+      <div className="flex items-center justify-center mx-auto px-4 pt-16 w-full gap-4">
+        {movies.map((movie) => (
+          <div
+            key={movie.id}
+            className="flex flex-col items-center justify-center gap-4"
+          >
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+              className="w-1/2 h-1/2 object-cover"
+            />
+            <h2>{movie.title}</h2>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ frontend/src/services/movieApi.tsx
@@ -0,0 +1,42 @@
+export const getMovies = async () => {
+  const url = "https://api.themoviedb.org/3/authentication";
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0M2I3MjA1MWM2YmJlOTZkYTkyNWQxZDFiZDY5ZjJkNiIsIm5iZiI6MTc1MzMwNjAzNC44MjIsInN1YiI6IjY4ODE1M2IyMjhhNmI4OTY1NmQ5YTE1NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XaOiuVhjtaSsB7eGl6SllVN4Ra6FLRKe4E5o37fnN1s",
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getFavoriteMovies = async () => {
+  const url =
+    "https://api.themoviedb.org/3/account/22169631/favorite/movies?language=en-US&page=1&sort_by=created_at.asc";
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0M2I3MjA1MWM2YmJlOTZkYTkyNWQxZDFiZDY5ZjJkNiIsIm5iZiI6MTc1MzMwNjAzNC44MjIsInN1YiI6IjY4ODE1M2IyMjhhNmI4OTY1NmQ5YTE1NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XaOiuVhjtaSsB7eGl6SllVN4Ra6FLRKe4E5o37fnN1s",
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    console.log(data.results);
+    return data.results;
+  } catch (error) {
+    console.error(error);
+  }
+};
