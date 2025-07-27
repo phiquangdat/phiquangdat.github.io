2dfda3704e234c41faa4da49cd560b5c4b57af78
phiquangdat <datphi020603@gmail.com>
2025-07-27 19:40:45 +0300

    Update package dependencies and refactor MoviePage component: add @vitejs/plugin-react, improve layout with MovieCard component, and enhance styling for better presentation.

---
+++ frontend/src/components/movies/MovieCard.tsx
@@ -0,0 +1,21 @@
+import { type Movie } from "./MoviePage";
+
+export default function MovieCard({ movie }: { movie: Movie }) {
+  return (
+    <div className="bg-white rounded-lg shadow-md flex flex-col items-center justify-center p-4 transition-transform hover:scale-105 group overflow-hidden">
+      <img
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        alt={movie.title}
+        className="object-cover mx-auto w-full h-100 rounded-md mb-4 transition-all duration-300"
+      />
+      <h2 className="font-bold text-lg mb-2">{movie.title || movie.name}</h2>
+      <p className="text-sm text-gray-500 line-clamp-3 mb-2">
+        {movie.overview}
+      </p>
+      <div className="flex items-center justify-center gap-2 mt-auto">
+        <span className="text-base text-gray-500 font-bold">Rating:</span>
+        <span className="text-sm text-gray-700">{movie.vote_average}/10</span>
+      </div>
+    </div>
+  );
+}
--- frontend/src/components/movies/MoviePage.tsx
+++ frontend/src/components/movies/MoviePage.tsx
@@ -1,5 +1,15 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { getFavoriteMovies, getRatedTV } from "../../services/movieApi";
+import MovieCard from "./MovieCard";
+
+export interface Movie {
+  id: number;
+  title: string;
+  name: string;
+  poster_path: string;
+  overview: string;
+  vote_average: number;
+}
 
 export default function MoviePage() {
   const [movies, setMovies] = useState<any[]>([]);
@@ -16,7 +26,7 @@ export default function MoviePage() {
 
   return (
     <div className="mx-auto px-4 pt-16 text-center">
-      <h1 className="text-center font-bold text-2xl text-[#172554]">
+      <h1 className="text-center font-bold text-3xl text-[#172554]">
         Movie Page
       </h1>
       <p className="text-center text-gray-500 mt-4">
@@ -24,43 +34,12 @@ export default function MoviePage() {
         common.
       </p>
 
-      <div className="grid grid-cols-2 md:grid-cols-3 mx-auto px-4 pt-16 w-full gap-4">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-8 mx-auto px-4 pt-16 w-full">
         {movies.map((movie) => (
-          <div
-            key={movie.id}
-            className="flex flex-col items-center justify-between max-w-xs mx-auto gap-4"
-          >
-            <img
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              alt={movie.title}
-              className="object-cover w-full h-2/3"
-            />
-            <h2 className="mt-2 font-bold">{movie.title}</h2>
-            <p className="text-sm text-gray-500 w-full line-clamp-3">
-              {movie.overview}
-            </p>
-            <span></span>
-          </div>
+          <MovieCard movie={movie} key={movie.id} />
         ))}
         {tv.map((tv) => (
-          <div
-            key={tv.id}
-            className="flex flex-col m-4 items-center justify-center gap-4"
-          >
-            <img
-              src={`https://image.tmdb.org/t/p/w500${tv.poster_path}`}
-              alt={tv.name}
-              className="object-cover"
-            />
-            <h2 className="mt-2 font-bold">{tv.name}</h2>
-            <p className="text-sm text-gray-500 line-clamp-3">{tv.overview}</p>
-            <div className="flex items-center justify-center gap-2">
-              <span className="text-lg text-gray-500 font-bold">
-                My Rating:
-              </span>
-              <span className="text-sm text-gray-500">{tv.rating}/10</span>
-            </div>
-          </div>
+          <MovieCard movie={tv} key={tv.id} />
         ))}
       </div>
     </div>
