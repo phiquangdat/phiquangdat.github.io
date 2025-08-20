a279858c8231a1c98fc7f17cca3c4f01e6b674f6
phiquangdat <datphi020603@gmail.com>
2025-08-21 00:10:24 +0300

    Add cover photo in HomeIntro

--- frontend/src/components/home/HomeIntro.tsx
+++ frontend/src/components/home/HomeIntro.tsx
@@ -2,8 +2,16 @@ import React from "react";
 
 const HomeIntro = () => {
   return (
-    <div className="mb-12 pt-8">
-      <div className="max-w-4xl mx-auto">
+    <div className="flex max-w-4xl mb-12 gap-6 items-center">
+      <div className="w-48 h-48 md:w-56 md:h-56 bg-gray-100 rounded-2xl border-2 border-gray-200 overflow-hidden p-4">
+        <img
+          src="https://img.freepik.com/premium-vector/vector-illustration-cute-wolf-drawing-colouring-activity_925324-10412.jpg?w=360"
+          alt="dat phi cover"
+          className="w-full h-full object-contain"
+        />
+      </div>
+
+      <div className="flex flex-col">
         <h1 className="text-4xl md:text-5xl font-bold text-gray-800 mb-4 font-serif">
           Dat Phi
         </h1>
