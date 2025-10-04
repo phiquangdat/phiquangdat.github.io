87d575f9a1569b2d37a85f49b5b835de3e2f8924
phiquangdat <datphi020603@gmail.com>
2025-10-04 11:37:31 +0300

    Cleanup project

--- frontend/src/utils/generateImageList.ts
+++
@@ -1,8 +0,0 @@
-const images = import.meta.glob("/src/assets/images/*.jpg", { eager: true });
-
-export const imageList = Object.values(images).map((mod: any) => {
-  const url = mod.default;
-  return process.env.NODE_ENV === "development"
-    ? `http://localhost:8010${url}`
-    : url;
-});
